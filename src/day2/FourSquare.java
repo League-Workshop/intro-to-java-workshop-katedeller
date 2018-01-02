bf0505e6a02811cea9e0ca5c6676278d2a5f2b69
katedeller <league@WTS10.attlocal.net>
2018-01-02 14:58:07 -0800

    Day one workshop

--- src/day2/FourSquare.java
+++ src/day2/FourSquare.java
@@ -6,27 +6,26 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot k9 = new Robot(); 
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+k9.setSpeed(100);
 		// 5. Set the pen width to 5
-
+k9.setPenWidth(5);
 		// 6. Do steps #7 to #8 four times...
 
 			// 7. Set the pen color to random
-	
+
 			// 1. Call the drawSquare() method
-	
+	drawSquare();
 			// 8. Turn the robot 90 degrees to the right
 
 	}
 
 	/* 3. Fill in the code to draw a square inside the method below. */
 	void drawSquare() {
-		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
-
-		
+		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");k9
+		for()
 		
 	}
 
--- src/day2/MyFirstJavaProgram.java
+++ src/day2/MyFirstJavaProgram.java
@@ -8,8 +8,15 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+	System.out.println("Hello World");
+	Robot k8 = new Robot();
+	k8.penDown();
+	k8.move(100);
+	k8.sparkle();
+	k8.setSpeed(100);
+	k8.penUp();
+	k8.moveTo(30, 40);
+	k8.turn(90);
 	
-		
-		
 	}
 }
