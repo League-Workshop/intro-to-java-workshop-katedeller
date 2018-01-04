1541cfbc204e380d9d558e064708bef8346cb8a4
katedeller <league@WTS-iMac-11.attlocal.net>
2018-01-04 14:53:22 -0800

    Day 4

--- src/day4/QuizGame.java
+++ src/day4/QuizGame.java
@@ -1,22 +1,40 @@
 package day4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String word = JOptionPane.showInputDialog("What is the capital of California?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(word.equals("Sacramento")) {
 		// 4.  if the user's answer is correct
-		
+			 speak("correct");
 		// -- add one to their score 
-		
+		score ++;}
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
+		word = JOptionPane.showInputDialog("What is the capital of America");
+		if(word.equals("Washington D.C.")) {
+			speak("correct");
+			score ++;}
+		word = JOptionPane.showInputDialog("What is the capital of New York");
+		if(word.equals("Albany")) {
+			speak("correct");
+			score ++;}
 		
 		// 6.  After all the questions have been asked, print the user's score 
-		
+		JOptionPane.showMessageDialog(null, "You scored " +score);
+	}
+	static void speak(String words) {
+		try {
+			Runtime.getRuntime().exec("say " + words).waitFor();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
--- src/day4/RobotTreasureHunt.java
+++ src/day4/RobotTreasureHunt.java
@@ -16,44 +16,53 @@ import org.jointheleague.graphical.robot.Robot;
 public class RobotTreasureHunt implements KeyEventDispatcher{
 
 	// 1. Create a new mini robot (type "mini" inside the parentheses)
-	
+	Robot k13 = new Robot("mini");
 	private void goUp() throws InterruptedException {
 		// 2. Make the robot move up the screen (use setAngle(angle) and microMove(distance))
-		
+		k13.setAngle(0);
+		k13.microMove(10);
 	}
 
 	private void goDown() throws InterruptedException{
 		// 3. make the robot move down the screen (use setAngle(angle) and microMove(distance))
-		
+		k13.setAngle(180);
+		k13.microMove(15);
 	}
 
 	private void turnLeft() throws InterruptedException{
 		// 4. Make the robot turn to the left (use setAngle(angle) and microMove(distance))
-
+k13.setAngle(-90);
+k13.microMove(10);
 	}
 
 	private void turnRight() throws InterruptedException{
 		// 5. make the robot turn to the right (use setAngle(angle) and microMove(distance))
-		
+		k13.setAngle(90);
+		k13.microMove(10);
 	}
 
 	private void spaceBarWasPressed() {
 
 		// 5. Change ROBOTNAME below to match the name of the robot you created in step 1.  THEN, remove the slashes at the beginning of the next two lines
-		//int robotXLocation = ROBOTNAME.getX();
-		//int robotYLocation = ROBOTNAME.getY();
+		
+		int robotXLocation = k13.getX();
+		int robotYLocation = k13.getY();
 		
 		// 6. Print the robotXLocation and robotYLocation variables to the console 
-		
+		System.out.println(robotXLocation +" "+ robotYLocation);
 		// 7. If robot is at same location as the little girl
 		//      --make a pop-up tell the robot where to go next
-		
+		if(robotXLocation == 720 && robotYLocation == 420) {
+			JOptionPane.showMessageDialog(null, "Go to pirate robot");
+		}
 		// 8. Give the user subsequent clues at different locations on the image
 		// (pirate robot, swamp, parrots, etc.)
 		
 		// 9.  If the robot is in the final location
 		//     --call the treasureFound() method
-		
+		if(robotXLocation == 450 && robotYLocation == 300) {
+		treasureFound();
+		}
 	}
 
 	private void go() {
